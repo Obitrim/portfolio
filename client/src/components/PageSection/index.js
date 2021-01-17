8a2a21d9d97900baea9913aa1dc4057e09436e85
obitrim <pevangelist516@gmail.com>
2021-01-17 15:48:47 +0000

    validated props

--- client/src/components/PageSection/index.js
+++ client/src/components/PageSection/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './PageSection.css';
 
 const Index = ({ bgColor, children, className, id, vWhitespace }) => {
@@ -15,6 +17,18 @@ const Index = ({ bgColor, children, className, id, vWhitespace }) => {
 			<div className="Container">{ children }</div>
 		</section>
 	)
+}
+
+Index.propTypes = {
+	bgColor: PropTypes.string,
+	children: PropTypes.node,
+	id: PropTypes.string,
+	vWhitespace: PropTypes.bool
+}
+
+Index.defaultProps = {
+	children: 'Insert section\'s content here',
+	vWhitespace: true
 }
 
 export default Index;
