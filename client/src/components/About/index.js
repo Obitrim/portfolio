cd6b7e695245d775f9da774cbecac50d882e34e3
obitrim <pevangelist516@gmail.com>
2021-01-17 15:46:29 +0000

    refactored markup

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,34 +1,41 @@
 import React from 'react';
+
+import './About.css';
 import PageSection from '../PageSection';
-import { Swiper, SwiperSlide } from 'swiper/react';
-// import SwiperCore, { Navigation, Pagination } from 'swiper';
-
-// Import Swiper styles
-// import 'swiper/swiper.scss';
-// import 'swiper/components/navigation/navigation.scss';
-// import 'swiper/components/pagination/pagination.scss';
-
-// install Swiper components
-// SwiperCore.use([Navigation, Pagination]);
-// import 'swiper/swiper.scss';
 
 const Index = (props) => {
   return (
-    <PageSection id="about" className="BgDark" vWhitespace={true}>
-    	 <Swiper
-	      spaceBetween={50}
-	      slidesPerView={3}
-	      navigation
-	      pagination={{ clickable: true }}
-	      scrollbar={{ draggable: true }}
-	      onSwiper={(swiper) => console.log(swiper)}
-	      onSlideChange={() => console.log('slide change')}
-	    >
-	      <SwiperSlide>Slide 1</SwiperSlide>
-	      <SwiperSlide>Slide 2</SwiperSlide>
-	      <SwiperSlide>Slide 3</SwiperSlide>
-	      <SwiperSlide>Slide 4</SwiperSlide>
-	    </Swiper>
+    <PageSection id="about" className="PageSection__About BgDark" vWhitespace={false}>
+    	<div className="About">
+    		<div className="About__Section About__Section--Bio">
+    		 	<h2 className="About__Section__Heading"><span>Who I am</span></h2>
+    		 	<p>Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Minus dolores dolor iure, maiores culpa possimus neque nobis odio, architecto, aliquam fugiat, doloremque adipisci enim inventore distinctio sapiente repellat officiis? Rem!</p>
+    		</div>
+	    	<div className="About__Section About__Section--Skills">
+	    	 	<h2 className="About__Section__Heading"><span>Skills</span></h2>
+	    	 	<ul className="About__Skills">
+	    	 		<li className="About__Skills__Item">Twitter</li>
+	    	 		<li className="About__Skills__Item">Facebook</li>
+	    	 		<li className="About__Skills__Item">LinkedIn</li>
+	    	 		<li className="About__Skills__Item">Instagram</li>
+	    	 	</ul>
+	    	</div>
+	    	<div className="About__Section About__Section--HowIStarted">
+	    	 	<h2 className="About__Section__Heading"><span>How I started</span></h2>
+	    	 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia impedit ipsam molestias repellendus velit exercitationem reiciendis placeat voluptatum sequi.</p>
+	    	 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia impedit ipsam molestias repellendus velit exercitationem reiciendis placeat voluptatum sequi.</p>
+	    	 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia impedit ipsam molestias repellendus velit exercitationem reiciendis placeat voluptatum sequi.</p>
+	    	</div>
+	    	<div className="About__Section About__Section--SocialMedia">
+	    	 	<h2 className="About__Section__Heading"><span>Follow Me</span></h2>
+	    	 	<ul className="About__SocialMedia">
+	    	 		<li className="About__SocialMedia__Item">Twitter</li>
+	    	 		<li className="About__SocialMedia__Item">Facebook</li>
+	    	 		<li className="About__SocialMedia__Item">LinkedIn</li>
+	    	 		<li className="About__SocialMedia__Item">Instagram</li>
+	    	 	</ul>
+	    	</div>
+    	</div>
     </PageSection>
   )
 }
